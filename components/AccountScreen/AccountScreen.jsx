8a2772ab0bdbde875afc8757940da003412e00ca
ManojKumar <Manojkumar850876@gmail.com>
2026-05-25 16:54:59 +0530

    update_product_json

--- components/AccountScreen/AccountScreen.jsx
+++ components/AccountScreen/AccountScreen.jsx
@@ -20,7 +20,7 @@ import Constants from 'expo-constants';
 const Account = ({ navigation }) => {
   const [userData, setUserData] = useState(null);
   const [loading, setLoading] = useState(true);
-  const LOGO_URL = Constants.expoConfig.extra?.LOGO_URL || "https://s3.ap-south-1.amazonaws.com/www.7miles.co.in/assets/Nav/7_miles_final_logo_PRINT_FILE-Photoroom.png";
+  // const LOGO_URL = Constants.expoConfig.extra?.LOGO_URL || "https://s3.ap-south-1.amazonaws.com/www.7miles.co.in/assets/Nav/7_miles_final_logo_PRINT_FILE-Photoroom.png";
   const PRODUCTS_IMAGE_API = Constants.expoConfig.extra?.PRODUCTS_IMAGE_API;
 
 
